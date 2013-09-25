055467d4cb9c5f25f670420c2f802ee2bd9bd084
Jeffrey C. Lee <leejeffc@gmail.com>
2013-09-25 13:12:55 -0700

    Implements functional stack.
    All functional stack tests pass.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,17 +3,26 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
 
   instance.push = function(value){
+    size++;
+    storage[size] = value;
   };
 
   instance.pop = function(){
+    if(size){
+      var result = storage[size];
+      delete storage[size];
+      size--;
+      return result;
+    }
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
