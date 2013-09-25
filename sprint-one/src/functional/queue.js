b82e2003bd9af3c2ec88a0bde1e80c05d0362d07
Jeffrey C. Lee <leejeffc@gmail.com>
2013-09-25 13:30:30 -0700

    Implements functional queue.
    All functional queue tests pass.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,31 @@ var makeQueue = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var size = 0;
+
   // Implement the methods below
 
   instance.enqueue = function(value){
+    for(var i = size; i > 0; i--){
+      if(storage.hasOwnProperty(i)){
+        storage[i + 1] = storage[i];
+      }
+    }
+    size++;
+    storage[1] = value;
   };
 
   instance.dequeue = function(){
+    if(size){
+      var result = storage[size];
+      delete storage[size];
+      size--;
+      return result;
+    }
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
