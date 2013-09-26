7b7f11462678c54a001875abf8a6891fe2d8fd25
Hack Reactor <hackreactor@Hacks-Mac-mini-2.local>
2013-09-26 11:19:46 -0700

    Adds tests to linked list spec and fixes incorrect syntax in the spec

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -17,28 +17,53 @@ describe("linkedList", function() {
   });
 
   // add more tests here to test the functionality of linkedList
-  it("should contain item after it is added", function(){
+  it("should contain node after it is added", function(){
     linkedList.addToTail('test string');
-    expect(linkedList.contains('test string').toEqual(true));
+    expect(linkedList.contains('test string')).toEqual(true);
   });
   
-  it("should no longer contain item after it is added and removed", function(){
+  it("should no longer contain node after it is added and removed", function(){
     linkedList.addToTail('test string');
     linkedList.removeHead('test string');
-    expect(linkedList.contains('test string').toEqual(false));
+    expect(linkedList.contains('test string')).toEqual(false);
   });
 
-  it("should still contain first item after adding another item", function(){
+  it("should still contain first node after adding another node", function(){
     linkedList.addToTail(1);
     linkedList.addToTail(2);
-    expect(linkedList.contains(1).toEqual(true));
+    expect(linkedList.contains(1)).toEqual(true);
   });
 
-  it("should contain only second item after adding 2 items and removing one of them", function(){
+  it("should return the value of node after it is removed", function(){
+    linkedList.addToTail('test string');
+    expect(linkedList.removeHead()).toEqual('test string');
+  })
+
+  it("should contain only second node after adding 2 nodes and removing one of them", function(){
     linkedList.addToTail(1);
     linkedList.addToTail(2);
     linkedList.removeHead();
-    expect(linkedList.contains(1).toEqual(false));
-    expect(linkedList.contains(2).toEqual(true));
+    expect(linkedList.contains(1)).toEqual(false);
+    expect(linkedList.contains(2)).toEqual(true);
+  });
+
+  it("should have remove the first nodes added to it", function(){
+    linkedList.addToTail(1);
+    linkedList.addToTail(2);
+    linkedList.addToTail(3);
+    expect(linkedList.contains(1)).toEqual(false);
+    linkedList.removeHead();
+    expect(linkedList.contains(2)).toEqual(false);
+    linkedList.removeHead();
+    expect(linkedList.contains(3)).toEqual(false);
+  });
+
+  it("should add and remove nodes correctly after calling removeHead on an empty list", function(){
+    linkedList.removeHead();
+    linkedList.addToTail(1);
+    linkedList.addToTail(2);
+    linkedList.removeHead();
+    expect(linkedList.contains(1)).toEqual(false);
+    expect(linkedList.contains(2)).toEqual(true);
   });
 });
