f6ff79182530348497ad69200dd62ba2333c4312
Hack Reactor <hackreactor@Hacks-Mac-mini-2.local>
2013-09-26 14:10:22 -0700

    Implements linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,13 +4,38 @@ var makeLinkedList = function(){
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(){
+  list.addToTail = function(value){
+    var newNode = makeNode(value);
+    
+    if(list.tail){
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
+
+    if(!list.head){
+      list.head = newNode;
+    }
   };
 
   list.removeHead = function(){
+    if(list.head){
+      var headValue = list.head.value;
+      list.head = list.head.next;
+      return headValue;
+    }
   };
 
-  list.contains = function(){
+  list.contains = function(target){
+    var currentNode = list.head;
+
+    while(currentNode){
+      if(currentNode.value === target){
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+
+    return false;
   };
 
   return list;
