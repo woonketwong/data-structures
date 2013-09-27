ae7c3c05d318a11a441f06e8be2feda8b3d66962
Hack Reactor <hackreactor@Hacks-Mac-mini-2.local>
2013-09-26 20:18:35 -0700

    Implements set with prototypal instantiation, adds tests to set spec, modifies SpecRunner.html to focus on set tests only

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -10,4 +10,41 @@ describe("set", function() {
     expect(set.contains).toEqual(jasmine.any(Function));
     expect(set.remove).toEqual(jasmine.any(Function));
   });
+
+  it("should contain a string after adding a string", function() {
+    set.add('test string');
+    expect(set.contains('test string')).toBe(true);
+  });
+
+  it("it should not contain any duplicates", function() {
+    set.add('test string');
+    set.add('test string');
+    set.remove('test string');
+    expect(set.contains('test string')).toBe(false);
+  });
+
+  it("it should contain many different strings after adding many different strings", function() {
+    set.add('hello');
+    set.add('world');
+    set.add('good');
+    set.add('bye');
+    expect(set.contains('hello')).toBe(true);
+    expect(set.contains('world')).toBe(true);
+    expect(set.contains('good')).toBe(true);
+    expect(set.contains('bye')).toBe(true);
+  });
+
+  it("it should not contain many different strings that were added and removed", function() {
+    set.add('hello');
+    set.add('world');
+    set.add('good');
+    set.add('bye');
+    set.remove('hello');
+    set.remove('good');
+    set.remove('bye');
+    expect(set.contains('hello')).toBe(false);
+    expect(set.contains('world')).toBe(true);
+    expect(set.contains('good')).toBe(false);
+    expect(set.contains('bye')).toBe(false);
+  });
 });
