6b8966b69fac26e554d89bc7d4a780e8d791d0e6
Hack Reactor <hackreactor@Hacks-Mac-mini-2.local>
2013-09-27 20:37:34 -0700

    Starts rough draft of doubly linked list.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -9,6 +9,7 @@ var makeLinkedList = function(){
     
     if(list.tail){
       list.tail.next = newNode;
+      newNode.previous = list.tail;
     }
     list.tail = newNode;
 
@@ -21,7 +22,31 @@ var makeLinkedList = function(){
     if(list.head){
       var headValue = list.head.value;
       list.head = list.head.next;
+      list.head.previous = null;
       return headValue;
+    }
+  };
+
+  list.addToHead = function(value){
+    var newNode = makeNode(value);
+    
+    if(list.head){
+      list.head.previous = newNode;
+      newNode.next = list.head;
+    }
+    list.head = newNode;
+
+    if(!list.head){
+      list.head = newNode;
+    }
+  };
+
+  list.removeTail = function(){
+    if(list.tail){
+      var tailValue = list.tail.value;
+      list.tail = list.tail.previous;
+      list.tail.next = null;
+      return tailValue;
     }
   };
 
@@ -45,6 +70,7 @@ var makeNode = function(value){
   var node = {};
   node.value = value;
   node.next = null;
+  node.previous = null;
 
   return node;
 };
