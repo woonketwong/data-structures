5c2e1e408db6bfb06b5c7d68dc01c110124c24dd
Hack Reactor <hackreactor@Hacks-Mac-mini-2.local>
2013-09-28 17:04:06 -0700

    Fixes insert method so it overwrites values of existing keys. Changes how resizing is initiated. Starts fixing resize feature to make it more efficient.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -19,9 +19,6 @@ HashTable.prototype.insert = function(k, v){
 
   var bucket = this._storage.get(i) || [];
 
-  // increment storageOccupancy if _storage[i] is an empty storage slot
-  !bucket.length && this._storageOccupancy++;
-
   var keyFound = false;
 
   _.each(bucket, function(bucketItem, bucketIndex, bucket){
@@ -33,6 +30,7 @@ HashTable.prototype.insert = function(k, v){
 
   if(!keyFound){
     bucket.push([k,v]);
+    this._storageOccupancy++;
   }
 
   this._storage.set(i, bucket);
@@ -44,22 +42,36 @@ HashTable.prototype.insert = function(k, v){
 };
 
 HashTable.prototype.resizeHashTable = function(resizeFactor){
-  var newLimit = this._limit * resizeFactor;
-  var newStorage = makeLimitedArray(newLimit);
-  var newStorageOccupancy = 0;
+  this._limit *= resizeFactor;
+  var oldStorage = this._storage;
+  this._storage = makeLimitedArray(this._limit);
 
-  this._storage.each(function(bucket, storageIndex, storage){
-    _.each(bucket, function(bucketItem, bucketIndex, bucket){
-      var i = getIndexBelowMaxForKey(bucketItem[0], newLimit);
-      var newBucket = newStorage.get(i) || [];
-      !newBucket.length && newStorageOccupancy++;
-      newBucket.push([bucketItem[0], bucketItem[1]]);
-      newStorage.set(i, newBucket);
-    });
+  oldStorage.each(function(bucket, storageIndex, storage){
+    if (bucket !== undefined){
+      for(var i = 0; i < bucket.length; i++){
+        console.log('this',this);
+        this.insert(bucket[i][0], bucket[i][1]);
+        console.log('after');
+      };
+    }
   });
-  this._limit = newLimit;
-  this._storage = newStorage;
-  this._storageOccupancy = newStorageOccupancy;
+
+  // var newLimit = this._limit * resizeFactor;
+  // var newStorage = makeLimitedArray(newLimit);
+  // var newStorageOccupancy = 0;
+
+  // this._storage.each(function(bucket, storageIndex, storage){
+  //   _.each(bucket, function(bucketItem, bucketIndex, bucket){
+  //     var i = getIndexBelowMaxForKey(bucketItem[0], newLimit);
+  //     var newBucket = newStorage.get(i) || [];
+  //     !newBucket.length && newStorageOccupancy++;
+  //     newBucket.push([bucketItem[0], bucketItem[1]]);
+  //     newStorage.set(i, newBucket);
+  //   });
+  // });
+  // this._limit = newLimit;
+  // this._storage = newStorage;
+  // this._storageOccupancy = newStorageOccupancy;
 };
 
 HashTable.prototype.retrieve = function(k){
@@ -79,22 +91,21 @@ HashTable.prototype.retrieve = function(k){
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage.get(i);
+  var bucket = this._storage.get(i) || [];
 
-  _.each(bucket, function(bucketItem, index, arr){
-    if(bucketItem[0] === k){
-      // Use Array.splice twice to remove bucketItem from the bucket
-      bucket = bucket.splice(0,index).concat(bucket.splice(index+1));
+  for(var j = 0; j < bucket.length; j++){
+    if(bucket[j][0] === k){
+      var removedVal = bucket.splice(j,1);
+      this._storageOccupancy--;
+      break;
     }
-  });
-  this._storage.set(i, bucket);
-
-  !bucket.length && this._storageOccupancy--;
+  }
 
   if(this._limit > 8 && this._storageOccupancy / this._limit <= 0.25){
     // resize
     this.resizeHashTable(1/2);
   }
+  return removedVal;
 };
 
 // NOTE: For this code to work, you will NEED the code from hashTableHelpers.js
